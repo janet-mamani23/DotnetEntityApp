157e52a2b0812c14a18913e8831ee625c0a37189
janetitaliamamani <janet.mamani@itscordoba.edu.ar>
2024-09-17 12:24:32 -0300

    se actualizo el archivo user.cs

--- entities_library/login/User.cs
+++ entities_library/login/User.cs
@@ -3,16 +3,9 @@ namespace entities_library.login;
 public class User : Person
 {
     public string? Password { get; set;}
-
-    public entities_library.file_system.File? File {get; set;}
-
-    public  string Avatar {get; set;}= "";
+    public entities_library.file_system.File? Avatar {get; set;}
     public string Description {get; set;} = "";
-
-    /*public List<ReportUser> ReportCounter {get; set;} = new List<ReportUser>(); //Va a llevar el conteo de reportes , si pasa 
-    los 3 reportes ese usuario sera banneado. Ademas de con la lista podremos acceder a detalles de cada reporte que se agrege a la lista.*/
-
-     public void Encrypt(string password)
+    public void Encrypt(string password)
     {
         this.Password = this.encrypt(password);
     } 
