c93ffe4f44f4bf41956b68b49d712845e16c49f5
Lara Gimenez <lara.gimenez@itscordoba.edu.ar>
2024-09-27 11:22:02 -0300

    Cambie el nombre de atributo genre

--- entities_library/movie/Movie.cs
+++ entities_library/movie/Movie.cs
@@ -1,6 +1,7 @@
 namespace entities_library.movie;
 using entities_library.comment;
 using entities_library.file_system;
+using entities_library.login;
 using entities_library.Qualify;
 
 public class Movie
@@ -9,10 +10,11 @@ public class Movie
     public required string Title {get; set;}
     public required string Description {get; set;}
     //Encapsulamiento: 
-    public required Genero Genre {get; set;}
+    public required Genre Genre {get; set;}
     //Genre es un atributo de Genero, osea Genero no es una propiedad de pelicula por que es un objeto. pelicula y genero son dos entities.
     public required FileEntity Image{get; set;}
     public required FileEntity Video{get; set;}
+    public required User User { get; set; }
     public Qualify? Star {get; set;}
     public List<Comment> Comments { get; set; } = new List<Comment>();
 
