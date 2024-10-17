24af33909568f43c2126ba8e6b973e39edbece5c
Lara Gimenez <lara.gimenez@itscordoba.edu.ar>
2024-10-16 23:35:41 -0300

    Cree el controller qualify

--- web_api/Controllers/QualifyController.cs
+++ web_api/Controllers/QualifyController.cs
@@ -1,11 +1,13 @@
-/*using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using web_api.dto.common;
 using web_api.dto.qualify;
 using web_api.mock;
 using entities_library.login;
+using dao_library.Interfaces;
+using dao_library.Interfaces.qualify;
 
 namespace web_api.Controllers;
-
+/*
 [ApiController] 
 [Route("[controller]")]
 
@@ -30,3 +32,48 @@ public class QualifyController : ControllerBase
             return new HttpStatusCodeResult(200, "Rating added/updated successfully");
         }
 }*/
+
+[ApiController]
+[Route("[controller]")]
+public class QualifyController : ControllerBase
+{
+    private readonly ILogger<QualifyController> _logger;
+    private readonly IDAOFactory daoFactory;
+
+    public QualifyController(
+        ILogger<LoginController> logger,
+        IDAOFactory daoFactory)
+    {
+        _logger = logger;
+        this.daoFactory = daoFactory;
+    }
+
+    [HttpPost(Name = "Qualify")]
+    public async Task<IActionResult> Post(QualifyRequestDTO qualifyRequestDTO)
+    {
+        IDAOQualify daoQualify = daoFactory.CreateDAOQualify();
+        
+        User user = await daoQualify.Get(
+            qualifyRequestDTO.Name,
+            qualifyRequestDTO.Star
+        );
+
+        if( qualifyRequestDTO != null &&
+            qualifyRequestDTO.IsPassword(loginRequestDTO.password))
+        {
+            return Ok(new QualifyResponseDTO
+            {
+                success = true,
+                message = "",
+                id = qualify.Id,
+                Start = qualify.Star
+            });
+        }
+        
+        return Unauthorized(new ErrorResponseDTO
+        {
+            success = false,
+            message = "Invalid mail or password"
+        });
+    }
+}
