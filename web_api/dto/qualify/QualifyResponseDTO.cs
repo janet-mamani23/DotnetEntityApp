b89713b37f650e67e89ab8883b4237b20d148c60
Lara Gimenez <lara.gimenez@itscordoba.edu.ar>
2024-10-24 19:47:05 -0300

    Agregada la referencia movie

--- web_api/dto/qualify/QualifyResponseDTO.cs
+++ web_api/dto/qualify/QualifyResponseDTO.cs
@@ -1,4 +1,5 @@
 using web_api.dto.common;
+using entities_library.movie;
 
 namespace web_api.dto.qualify;
 
@@ -6,6 +7,5 @@ public class QualifyResponseDTO : ResponsetDTO
 {
     public long Id{get;set;}
     public required int Star {get; set;}
-
     public required int AverageStars {get; set;}
 }
