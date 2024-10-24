667fc977d1d8e312c5dc48d9c7e653c3866e2e61
Lara Gimenez <lara.gimenez@itscordoba.edu.ar>
2024-10-24 19:41:42 -0300

    Agregado el metodo Update, GetByMovieId y HasUserQualifiedMovie para obtener las calificaciones del usuario

--- dao_library/Interfaces/qualify/IDAOQualify.cs
+++ dao_library/Interfaces/qualify/IDAOQualify.cs
@@ -3,11 +3,15 @@ using entities_library.Qualify;
 namespace dao_library.Interfaces.qualify;
 
 public interface IDAOQualify
-{
-    Task<IEnumerable<Qualify>> GetAll();
+{   
     Task<Qualify> GetById(long id);
     Task Save(Qualify qualify);
     
     //NO PROGRAMAR
-    Task Delete(Qualify qualify);
+    public Task Delete(long id);
+
+    Task<bool> Update(Qualify qualify);
+    Task<IEnumerable<Qualify>> GetByMovieId(long movieId);
+
+    Task <bool> HasUserQualifiedMovie(long userId, long movieId);
 }
