1f3db911d595d0e303d486b6196621cd8db33f45
Lara Gimenez <lara.gimenez@itscordoba.edu.ar>
2024-10-24 21:50:09 -0300

    Se agregaro propiedad Delete, Update, GetByMovieId y HasUserQualifiedMovie

--- dao_library/Interfaces/qualify/IDAOQualify.cs
+++ dao_library/Interfaces/qualify/IDAOQualify.cs
@@ -1,12 +1,19 @@
 using entities_library.Qualify;
 
+
 namespace dao_library.Interfaces.qualify;
 
+
 public interface IDAOQualify
-{
+{  
     Task<Qualify> GetById(long id);
     Task Save(Qualify qualify);
-    
-    //NO PROGRAMAR
-    Task Delete(Qualify qualify);
+
+   //NO PROGRAMAR
+    public Task Delete(long id);
+
+
+    Task<bool> Update(Qualify qualify);
+    Task<IEnumerable<Qualify>> GetByMovieId(long movieId);
+    Task <bool> HasUserQualifiedMovie(string Useremail, long movieId);
 }
