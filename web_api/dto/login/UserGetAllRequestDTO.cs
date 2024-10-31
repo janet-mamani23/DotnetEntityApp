015d0c03542f43ec8fc52fda21c4c3cf42b2f48f
Lara Gimenez <lara.gimenez@itscordoba.edu.ar>
2024-10-31 00:52:26 -0300

    Puse la primera letra en mayuscula

--- web_api/dto/login/UserGetAllRequestDTO.cs
+++ web_api/dto/login/UserGetAllRequestDTO.cs
@@ -4,7 +4,7 @@ namespace web_api.dto.login;
 
 public class UserGetAllRequestDTO : RequestDTO
 {
-    public string? query { get; set; }= null;
-    public int page { get; set; } = 1;
-    public int pageSize { get; set; } = 10;
+    public string? Query { get; set; }= null;
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
 }
