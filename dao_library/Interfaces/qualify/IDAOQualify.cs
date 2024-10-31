360f4153c415a31b2d1cb6d25d03ee4564b87742
Lara Gimenez <lara.gimenez@itscordoba.edu.ar>
2024-10-31 01:03:44 -0300

    Añadi GetQualifyByUserAndMovie

--- dao_library/Interfaces/qualify/IDAOQualify.cs
+++ dao_library/Interfaces/qualify/IDAOQualify.cs
@@ -7,13 +7,16 @@ namespace dao_library.Interfaces.qualify;
 public interface IDAOQualify
 {  
     Task<Qualify> GetById(long id);
+
     Task Save(Qualify qualify);
 
    //NO PROGRAMAR
     public Task Delete(long id);
 
-
     Task<bool> Update(Qualify qualify);
     Task<IEnumerable<Qualify>> GetByMovieId(long movieId);
-    Task <bool> HasUserQualifiedMovie(string Useremail, long movieId);
+
+    Task<Qualify?> GetQualifyByUserAndMovie(long userId, long movieId);
+
+    Task <bool> HasUserQualifiedMovie(string Useremail, long movieId); //comprueba si el usuario ya calificó la película
 }
