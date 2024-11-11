83ce3aeadd27622fc8dc216dd3a572d8c5607780
Lara Gimenez <lara.gimenez@itscordoba.edu.ar>
2024-11-11 19:07:08 -0300

    modifique la inicial de los atributos

--- web_api/dto/qualify/QualifyRequestDTO.cs
+++ web_api/dto/qualify/QualifyRequestDTO.cs
@@ -6,7 +6,7 @@ namespace web_api.dto.qualify;
 
 public class QualifyRequestDTO : RequestDTO
 {
-    public required Person Name {get; set;}
+    public required int userId {get; set;}
 
     public required int Star {get; set;}
 
