a030238c3488ce64c9da4f513e1f734a4fed487c
janetitaliamamani <janet.mamani@itscordoba.edu.ar>
2025-02-26 15:22:37 -0300

    se creo una nueva migrations

---
+++ dao_library/Migrations/20250226114536_ActualizacionMovie.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace daolibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizacionMovie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
